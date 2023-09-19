6863cededcf1ea0f0fc66136fd7e598e045ef004
Eddú Meléndez <eddu.melendez@gmail.com>
2023-09-19 11:12:19 -0600

    Polish

--- src/main/java/io/sdkman/automigration/config/RestTemplateConfig.java
+++ src/main/java/io/sdkman/automigration/config/RestTemplateConfig.java
@@ -1,11 +1,16 @@
 package io.sdkman.automigration.config;
 
-import io.sdkman.automigration.infrastructure.NoRedirectSimpleClientHttpRequestFactory;
-import io.sdkman.automigration.infrastructure.ServerResponseErrorHandler;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
 
 @Configuration
 public class RestTemplateConfig {
@@ -15,6 +20,31 @@ public class RestTemplateConfig {
 		return restTemplateBuilder.requestFactory(NoRedirectSimpleClientHttpRequestFactory.class)
 			.errorHandler(new ServerResponseErrorHandler())
 			.build();
+	}
+
+	static class NoRedirectSimpleClientHttpRequestFactory extends SimpleClientHttpRequestFactory {
+
+		@Override
+		protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws IOException {
+			super.prepareConnection(connection, httpMethod);
+			connection.setInstanceFollowRedirects(false);
+		}
+
+	}
+
+	static class ServerResponseErrorHandler extends DefaultResponseErrorHandler {
+
+		@Override
+		protected boolean hasError(int unknownStatusCode) {
+			HttpStatus.Series series = HttpStatus.Series.resolve(unknownStatusCode);
+			return series == HttpStatus.Series.SERVER_ERROR;
+		}
+
+		@Override
+		protected boolean hasError(HttpStatusCode statusCode) {
+			return statusCode.is5xxServerError();
+		}
+
 	}
 
 }
--- src/main/java/io/sdkman/automigration/models/FoojayQueryParams.java
+++ src/main/java/io/sdkman/automigration/models/FoojayQueryParams.java
@@ -1,5 +1,13 @@
 package io.sdkman.automigration.models;
 
-public record FoojayQueryParams(String distribution, String version, String javaVersion, String releaseStatus,
-		String os, boolean javafxBundled, String features, VendorProperties.OS vendorOsProperties) {
+// @formatter:off
+public record FoojayQueryParams(String distribution,
+								String version,
+								String javaVersion,
+								String releaseStatus,
+								String os,
+								boolean javafxBundled,
+								String features,
+								VendorProperties.OS vendorOsProperties) {
 }
+// @formatter:on
