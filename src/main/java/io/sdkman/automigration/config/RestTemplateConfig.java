6f4475e07731fd41cb279ea56cb50c327a694055
Eddú Meléndez <eddu.melendez@gmail.com>
2023-09-19 18:20:27 -0600

    Polish

--- src/main/java/io/sdkman/automigration/config/RestTemplateConfig.java
+++ src/main/java/io/sdkman/automigration/config/RestTemplateConfig.java
@@ -3,7 +3,6 @@ package io.sdkman.automigration.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.DefaultResponseErrorHandler;
@@ -33,12 +32,6 @@ public class RestTemplateConfig {
 	}
 
 	static class ServerResponseErrorHandler extends DefaultResponseErrorHandler {
-
-		@Override
-		protected boolean hasError(int unknownStatusCode) {
-			HttpStatus.Series series = HttpStatus.Series.resolve(unknownStatusCode);
-			return series == HttpStatus.Series.SERVER_ERROR;
-		}
 
 		@Override
 		protected boolean hasError(HttpStatusCode statusCode) {
